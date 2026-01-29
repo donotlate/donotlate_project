1605cfdea8060359c9ddc5ade08d1f707afc7131
ycm93277211-ai <ycm93277211@gmail.com>
2026-01-29 16:54:20 +0900

    no message

--- donotlate_project/src/main/java/com/late/donot/admin/model/mapper/AdminMapper.java
+++ donotlate_project/src/main/java/com/late/donot/admin/model/mapper/AdminMapper.java
@@ -1,7 +1,5 @@
 package com.late.donot.admin.model.mapper;
 
-import java.util.List;
-
 import org.apache.ibatis.annotations.Mapper;
 
 import com.late.donot.member.model.dto.Member;
@@ -10,7 +8,5 @@ import com.late.donot.member.model.dto.Member;
 public interface AdminMapper {
 
 	Member login(String memberEmail);
-
-	List<Member> getUsers(Member member);
 
 }
