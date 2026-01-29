cd5961aff0cf41e39c4f5fcdeb0b257c388abd05
chulkuji12 <chulkuji12@gmail.com>
2026-01-29 10:19:35 +0900

    자외선 추가

--- donotlate_project/src/main/java/com/late/donot/api/dto/UltravioletWeather.java
+++ donotlate_project/src/main/java/com/late/donot/api/dto/UltravioletWeather.java
@@ -1,16 +1,16 @@
-//package com.late.donot.api.dto;
-//
-//import lombok.AllArgsConstructor;
-//import lombok.Builder;
-//import lombok.Data;
-//import lombok.NoArgsConstructor;
-//
-//@Data
-//@AllArgsConstructor
-//@NoArgsConstructor
-//@Builder
-//public class UltravioletWeather {
-//	private int uvIndex;
-//    private String level;
-//	
-//}
+package com.late.donot.api.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UltravioletWeather {
+	private int uvIndex;
+    private String level;
+	
+}
